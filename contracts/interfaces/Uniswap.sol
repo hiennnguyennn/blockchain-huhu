7a4b798b83dda9bcf466bfffbd80e8be79b50804
hiennnguyennn <bkhrjscnjh@gmail.com>
2022-07-11 02:58:59 +0700

    update

--- contracts/interfaces/Uniswap.sol
+++ contracts/interfaces/Uniswap.sol
@@ -2,14 +2,15 @@
 pragma solidity >=0.6.2;
 
 interface IUniswapV2RRouter {
-    function swapExtractTokensForTokens(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path,
-        address to,
-        uint256 deadline
-    ) external returns (uint256[] memory amounts);
+    function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
     function getAmountsOut(uint amountIn, address[] memory path) external view returns (uint[] memory amounts);
     function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)  external  payable  returns (uint[] memory amounts);
   function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)  external  returns (uint[] memory amounts);
+  function swapExactTokensForTokensSupportingFeeOnTransferTokens( uint amountIn,  uint amountOutMin,  address[] calldata path,  address to,  uint deadline) external;
 }
--- test/test-uniswap.js
+++ test/test-uniswap.js
@@ -1,46 +1,26 @@
 // SPDX-License-Identifier: MIT
 const IERC20 = artifacts.require("IERC20");
 const TestUniswap = artifacts.require("TestUniswap");
-contract('TestUniswap', (accounts) => {
+
+contract('TestUniswap', async(accounts) => {
   const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
   const WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
   const BUSD = "0x4Fabb145d64652a948d72533023f6E7A623C7C53";
   const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
   const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-  const ETH_WHALE="0x8f33edEF2Cf3BdF9B082350cA72a1a09F8d36EBA";
-  //const WHALE = WETH_WHALE;
-  const AMOUNT_IN = 1000000;
+  const DAI_WHALE="0xF977814e90dA44bFA03b6295A0616a897441aceC";
+  const ETH_WHALE="0xDC1403aC8313eBaD89acD204092Dac65Ea81Fc40";
+  
+  const AMOUNT_IN = 1000;
   const AMOUNT_OUT_MIN = 1;
  const TOKEN_IN = DAI;
-  const TOKEN_OUT = WETH;
-  const TO = ETH_WHALE
-  const WHALE=ETH_WHALE;
-
- /*
-   it("swap token to token", async () => {
-    tokenIn = await IERC20.at(TOKEN_IN);
-    tokenOut = await IERC20.at(TOKEN_OUT);
-    testUniswap = await TestUniswap.new();
-    console.log(`in ${await tokenIn.balanceOf(TO)}`,AMOUNT_IN);
-    await tokenIn.approve(testUniswap.address, AMOUNT_IN, { from: WHALE });
-    console.log(tokenIn.address, tokenOut.address, AMOUNT_IN, AMOUNT_OUT_MIN, WHALE, TO)
-     await testUniswap.swap(
-       tokenIn.address,
-       tokenOut.address,
-       AMOUNT_IN,
-       AMOUNT_OUT_MIN,
-       TO,
-       {
-        from: WHALE,
-       }
-     );
+  const TOKEN_OUT= WBTC;
+  const TO = accounts[0];
+  const WHALE=accounts[0];
   
-    
-     console.log(`in ${AMOUNT_IN}`);
-     console.log(`out ${await tokenOut.balanceOf(TO)}`);
-   });
-   */
-   
+ 
+  
+  /* 
   it("should get amount out min",async()=>{
     tokenIn = await IERC20.at(TOKEN_IN);
     tokenOut = await IERC20.at(TOKEN_OUT);
@@ -51,35 +31,59 @@ contract('TestUniswap', (accounts) => {
     console.log(`in ${AMOUNT_IN} and x: `, x.toNumber());
     
   }) 
+  */
   /*
   it("eth to token",async()=>{
-    tokenOut = await IERC20.at(TOKEN_OUT);
-    testUniswap = await TestUniswap.new();
-   await testUniswap.swapFromETHToToken(tokenOut.address,AMOUNT_OUT_MIN,TO,{value:AMOUNT_IN, FROM:WHALE});
-   console.log(`in ${AMOUNT_IN}`);
+    tokenOut = await IERC20.at(TOKEN_OUT); 
+    let testUniswap = await TestUniswap.new();
+   await testUniswap.swapFromETHToToken(tokenOut.address,AMOUNT_OUT_MIN,TO,{value:AMOUNT_IN, FROM:TO});
+   console.log(`in ${AMOUNT_IN}`, WHALE);
     console.log(`out ${await tokenOut.balanceOf(TO)}`);
   })
   */
- /*
- it("token to eth",async()=>{
+  /*
+  it("swap token to token", async () => {
+    tokenIn = await IERC20.at(TOKEN_IN);
+    tokenOut = await IERC20.at(TOKEN_OUT);
+    testUniswap = await TestUniswap.new();
+    console.log(`in ${await tokenIn.balanceOf(TO)}`,AMOUNT_IN);
+    await tokenIn.approve(testUniswap.address, AMOUNT_IN, { from: TO });
+    console.log(tokenIn.address, tokenOut.address, AMOUNT_IN, AMOUNT_OUT_MIN, WHALE, TO)
+     await testUniswap.swap(
+       tokenIn.address,
+       tokenOut.address,
+       AMOUNT_IN,
+       AMOUNT_OUT_MIN,
+       TO,
+       {
+        from: TO,
+       }
+     );
+  
+    
+     console.log(`in ${AMOUNT_IN} and ${await tokenIn.balanceOf(TO)}`);
+     console.log(`out ${await tokenOut.balanceOf(TO)}`);
+   });
+   */
+/*
+
+it("token to eth",async()=>{
   tokenIn = await IERC20.at(TOKEN_IN);
   testUniswap = await TestUniswap.new();
   console.log(`in ${await tokenIn.balanceOf(TO)}`,AMOUNT_IN);
-  await tokenIn.approve(testUniswap.address, AMOUNT_IN, { from: WHALE });
-  console.log(tokenIn.address,AMOUNT_IN, AMOUNT_OUT_MIN, WHALE, TO)
+  await tokenIn.approve(testUniswap.address, AMOUNT_IN, { from: TO });
+
    await testUniswap.swapFromTokenToETH(
-     tokenIn.address,
+    token_eth.address,
      AMOUNT_IN,
      AMOUNT_OUT_MIN,
      TO,
      {
-      from: WHALE,
+      from: TO,
      }
    );
 
-  
-   console.log(`in ${AMOUNT_IN}`);
-   console.log(`out ${await tokenOut.balanceOf(TO)}`);
+   console.log(`out ${await token_eth.balanceOf(TO)}`);
  })
  */
 });
